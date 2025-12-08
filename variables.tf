27b8bd30db6404171763dc1ec8df0a478f18bbbd
Leon-Aera <liang.ren.ca@gmail.com>
2025-12-08 09:46:04 -0800

    Clean up Terraform repo and sync local changes

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 variable "ecs_demo_image" {
-  description = "ECR image URI for ECS demo (e.g. 123456789012.dkr.ecr.us-west-2.amazonaws.com/ecs-demo:latest)"
+  description = "ECR image URI for ECS demo (e.g. 895169747731.dkr.ecr.us-east-1.amazonaws.com/ecs-demo:latest)"
   type        = string
 }
 
 variable "ecs_desired_count" {
   description = "How many ECS tasks to run"
   type        = number
-  default     = 2
+  default     = 3
 }
